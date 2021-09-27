5d31fbc232fcb187739ba1c63667d06f22459a8d
Snehal Patil <sneha.swapnil1996@gmail.com>
2021-09-27 22:44:05 +0530

    UC 1 is added

---
+++ Emp_Wage_UC1_Day9/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Emp_Wage_UC1_Day8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeAttendance.GetAttendance();
+        }
+    }
+}
