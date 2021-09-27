eed82a6db87c68875d23deb329e840727c7154c3
Snehal Patil <sneha.swapnil1996@gmail.com>
2021-09-27 22:51:39 +0530

    UC7 is added

---
+++ Emp_Wage_UC7_Day9/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Emp_Wage_UC7_Day8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UC7_ComputeEmpWageUsingClassMethod.ComputeEmpWage();
+        }
+    }
+}
