f308500335071e04a8a8bf03d8fe3a7cdcb00080
Snehal Patil <sneha.swapnil1996@gmail.com>
2021-09-27 22:48:37 +0530

    UC4 is added

---
+++ Emp_Wage_UC4_Day9/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Emp_Wage_UC4_Day8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmplyeeWageUsingSwitchCase.EmpWageUsingSwitch();
+        }
+    }
+}
