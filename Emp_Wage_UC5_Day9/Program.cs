64a3ef5ec1a6dae3d02c03b7889a10260f45b781
Snehal Patil <sneha.swapnil1996@gmail.com>
2021-09-27 22:49:39 +0530

    UC5 is added

---
+++ Emp_Wage_UC5_Day9/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Emp_Wage_UC5_Day8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UC5_CalculateWagesFor20DaysInMonth.CalculateWagesFor20DaysInMonth();
+        }
+    }
+}
