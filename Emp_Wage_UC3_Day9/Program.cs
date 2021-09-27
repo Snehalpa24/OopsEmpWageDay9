83f32b07ad9554bc81bbd2b01e746ee4257cc65d
Snehal Patil <sneha.swapnil1996@gmail.com>
2021-09-27 22:46:33 +0530

    UC3 is added

---
+++ Emp_Wage_UC3_Day9/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Emp_Wage_UC3_Day8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uc3_PartTimeempwage.PartTimeEmpWage();
+        }
+    }
+}
