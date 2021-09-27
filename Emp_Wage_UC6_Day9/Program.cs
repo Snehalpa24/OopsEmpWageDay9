0ede5712b098a647263eec76b114ecc501a4c513
Snehal Patil <sneha.swapnil1996@gmail.com>
2021-09-27 22:50:36 +0530

    UC6 is added

---
+++ Emp_Wage_UC6_Day9/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Emp_Wage_UC6_Day8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UC6_CalculateWageTill100HrsOr20IsReached.CalculateWageTill100HrsOr20IsReached();
+        }
+    }
+}
