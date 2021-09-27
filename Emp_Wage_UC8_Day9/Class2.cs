980db72b401f9159589d3f8fae47183c67a6e843
Snehal Patil <sneha.swapnil1996@gmail.com>
2021-09-27 23:18:20 +0530

    UC8 is added

---
+++ Emp_Wage_UC8_Day9/Class2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Emp_Wage_UC8_Day9
+{
+    class Class2
+    {
+        IS_PART_TIME=1;
+        IS_FULL_TIME=2;
+        MAX_HRS_IN_MONTH=4;
+        EMP_RATE_PER_HRS=20;
+        NUM_WORKING_DAYS=20;
+
+totalEmpHr=0;
+totalworkingday=0;
+
+function getWorkingHours()
+        {
+            local empCheck =$1
+   case $empCheck in
+      $IS_FULL_TIME)
+          workHours = 12
+           ; ;
+       $IS_PART_TIME)
+          workHours = 8
+           ; ;
+            *)
+          workHours = 0
+          ; ;
+            esac
+            echo $workHours
+}
+
+
+
+
+
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalworkingday -lt $NUM_WORKING_DAYS]]
+{
+   ((totalworkingday++))
+    empCheck=$((RANDOM%3))
+    empHrs="$( getWorkingHours $empCheck )"
+    totalEmpHrs=$(($totalEmpHrs+$empHrs))
+    dailyWage[$totalworkingday]=$(($empHrs* $EMP_RATE_PER_HRS))
+}
+
+totalSalary =$(($totalEmpHrs* $EMP_RATE_PER_HRS))
+echo "Total Employee wage="$totalSalary
+echo ${dailyWage[@]
+    }
+}
+
+}
+}
---
+++ Emp_Wage_UC8_Day9/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Emp_Wage_UC8_Day9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class2.getWorkingHours();
+        }
+    }
+}
